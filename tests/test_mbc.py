e037dac2ac0c62b7bca38d447047d2e4094600bc
Jeff Crawford <jcrawford888@gmail.com>
2019-04-20 15:06:21 -0700

    Add some more tests for missing params

--- tests/test_mbc.py
+++ tests/test_mbc.py
@@ -1,5 +1,8 @@
+import pytest
 import mbc
 import os
+
+url = "http://www.unmuseum.org/bealepap.htm"
 
 
 def test_version():
@@ -12,8 +15,6 @@ def test_encrypt_decrypt_file():
     fh = open(infile_name, "w")
     fh.write(clear_text)
     fh.close()
-
-    url = "http://www.unmuseum.org/bealepap.htm"
 
     outdata = mbc.encrypt(infile_name, url)
     outfilename = "mbctest.mbc"
@@ -32,10 +33,49 @@ def test_encrypt_decrypt_file():
 
 def test_encrypt_decrypt_string():
     clear_text = "This is a secret message."
-    url = "http://www.unmuseum.org/bealepap.htm"
 
     cipher_arr = mbc.encrypts(clear_text, url)
 
     msg = mbc.decrypts(cipher_arr, url)
 
     assert msg == clear_text.lower()
+
+
+def test_encrypts_missing_message():
+    with pytest.raises(ValueError):
+        mbc.encrypts("", url)
+
+
+def test_encrypts_missing_url():
+    with pytest.raises(ValueError):
+        mbc.encrypts("Hello World", "")
+
+
+def test_encrypt_missing_filename():
+    with pytest.raises(ValueError):
+        mbc.encrypt("", url)
+
+
+def test_encrypt_missing_url():
+    with pytest.raises(ValueError):
+        mbc.encrypt("filename.txt", "")
+
+
+def test_decrypts_missing_arr():
+    with pytest.raises(ValueError):
+        mbc.decrypts([], url)
+
+
+def test_decrypts_missing_url():
+    with pytest.raises(ValueError):
+        mbc.decrypts([0], "")
+
+
+def test_decrypt_missing_filename():
+    with pytest.raises(ValueError):
+        mbc.decrypt("", url)
+
+
+def test_decrypt_missing_url():
+    with pytest.raises(ValueError):
+        mbc.decrypt("filename.txt", "")
