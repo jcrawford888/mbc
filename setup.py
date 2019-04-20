14cb961059cbaf5fb55a72740fd466e2713d7b0a
Jeff Crawford <jcrawford888@gmail.com>
2019-04-19 20:09:51 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mbc",
+    version="0.0.1",
+    author="Jeff Crawford",
+    author_email="jcrawford888@gmail.com",
+    description="A Modernized Beale Cipher encryption/decryption module",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jcrawford888/mbc",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=['requests','html2text'],
+    python_requires="~3.6"
+)
